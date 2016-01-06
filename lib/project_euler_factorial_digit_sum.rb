6ff1e8965f1424dd5106664a95d80d7587101dfa
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-06 00:14:21 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,21 @@
 # code your solution here
+
+def factorial(number)
+  integers = (1..number)
+  factorial = integers.inject { |product, n| product * n }
+  
+  factorial
+end
+
+def sum_of_digits(number)
+  digits = number.to_s.chars.map(&:to_i)
+  total = digits.inject { |sum, digit|  sum + digit }
+  
+  total
+end
+
+def factorial_digit_sum(number)
+  sum = sum_of_digits(factorial(number))
+  
+  sum
+end
